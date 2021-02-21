0137cd0f504855ba294670d770a0669601e5fc05
carlos.sempe <carlos.sempe@zenvia.com>
2021-02-21 09:37:54 -0300

    first commit

---
+++ src/main/java/com/learn/kafka/LibraryProducerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProducerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProducerKafkaApplication.class, args);
+	}
+
+}
